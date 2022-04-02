2bec75b3f07ea0a9bc86c32d7dd5a2ef8dd64950
Кирилл Коннов <k-konnov@list.ru>
2022-04-02 22:46:11 +0400

    Создан репозиторий WorkoutRepository.kt для доступа к тренировкам

---
+++ app/src/main/java/com/example/fithelper/Repositories/WorkoutRepository.kt
@@ -0,0 +1,46 @@
+package com.example.fithelper.Repositories
+
+import android.util.Log
+import com.example.fithelper.Models.Workout
+import com.google.firebase.firestore.FirebaseFirestore
+
+object WorkoutRepository {
+    private val workouts = FirebaseFirestore.getInstance().collection("Workouts")
+
+    fun getWorkoutByUserId(userId: String): MutableList<Workout> {
+        val result = mutableListOf<Workout>()
+        workouts.whereEqualTo("userId", userId)
+            .get()
+            .addOnSuccessListener { snapshot ->
+                for (workout in snapshot) {
+                    result.add(workout.toObject(Workout::class.java))
+                }
+            }
+            .addOnFailureListener { ex ->
+                Log.e("Database", ex.message.toString())
+            }
+
+        return result
+    }
+
+    fun createWorkout(workout: Workout) {
+        workouts
+            .add(workout)
+            .addOnSuccessListener {
+                Log.i("Database", "Workout created")
+            }
+            .addOnFailureListener { ex ->
+                Log.e("Database", ex.message.toString())
+            }
+    }
+
+    fun updateWorkout(workout: Workout) {
+        workouts.document(workout.id!!)
+            .set(workout)
+    }
+
+    fun deleteWorkoutByUser(workoutId: String) {
+        workouts.document(workoutId).delete()
+    }
+
+}
