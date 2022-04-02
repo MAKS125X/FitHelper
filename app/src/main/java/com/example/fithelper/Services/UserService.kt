5a7fb91d89342992c3522ed9558dd60794cd996e
Кирилл Коннов <k-konnov@list.ru>
2022-04-02 22:45:46 +0400

    Добавлен UserService.kt

---
+++ app/src/main/java/com/example/fithelper/Services/UserService.kt
@@ -0,0 +1,12 @@
+package com.example.fithelper.Services
+
+import com.google.firebase.auth.FirebaseAuth
+
+object UserService {
+
+    private val auth = FirebaseAuth.getInstance()
+    fun getUserId(): String {
+        val user = auth.currentUser ?: return ""
+        return user.uid;
+    }
+}
